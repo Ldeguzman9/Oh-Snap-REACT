4e941edaaab1daa4abe00471693dfafc05c2d172
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-02-17 19:30:31 -0800

    modal

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", () => {
-  // Render About test
-  // First Test
-  it("renders", () => {
+describe('About component', () => {
+  it('renders', () => {
     render(<About />);
   });
 
-  // Second Test
-  it("matches snapshot DOM node structure", () => {
-    //renders About
+  it('matches snapshot', () => {
     const { asFragment } = render(<About />);
+
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
